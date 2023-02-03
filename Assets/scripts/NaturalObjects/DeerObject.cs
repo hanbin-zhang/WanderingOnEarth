177f9edcd80a557d9c24bd672ae2ada82f689a6a
HBZT <78476705+hanbin-zhang@users.noreply.github.com>
2023-02-03 16:52:05 +0000

    new natural object

--- Assets/scripts/NaturalObjects/DeerObject.cs
+++ Assets/scripts/NaturalObjects/DeerObject.cs
@@ -2,17 +2,22 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class NaturalObject : MonoBehaviour
+public class DeerObject : NaturalObject
 {
     // Start is called before the first frame update
     void Start()
     {
-        
+        this.greenValue = 6.0f;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetKeyDown(KeyCode.P))
+        {
+            
+            this.UpdateObject();
+            this.UpdateState();
+        }
     }
 }
---
+++ Assets/scripts/NaturalObjects/NaturalObject.cs
@@ -0,0 +1,51 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NaturalObject : MonoBehaviour
+{
+
+    public List<GameObject> Prefabs;
+
+    public float greenValue; 
+    int currentState = 0;
+    GameObject currentModel;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void UpdateObject()
+    {
+        if (currentModel != null)
+        {
+            Destroy(currentModel);
+        }
+
+        currentModel = Instantiate(Prefabs[currentState], transform);
+
+        currentModel.transform.localPosition = this.transform.localPosition;
+    }
+
+    public void UpdateState()
+    {
+        if (currentState < Prefabs.Count - 1)
+        {
+            currentState++;
+        }
+        else
+        {   
+            currentState = 0;
+        }
+    }
+
+}
