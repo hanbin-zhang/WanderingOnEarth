39e8cc4fec40046c336add6ad80a98af8bcfcc7f
HBZT <78476705+hanbin-zhang@users.noreply.github.com>
2023-02-06 17:47:48 +0000

    add a tree prefab

--- Assets/scripts/NaturalObjects/TreeObject.cs
+++ Assets/scripts/NaturalObjects/TreeObject.cs
@@ -2,19 +2,24 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class DeerObject : NaturalObject
+public class TreeObject : NaturalObject
 {
     // Start is called before the first frame update
     void Start()
     {
-        this.greenValue = 6.0f;
-        //this.currentModel = Prefabs[0];
+        this.greenValue = 1.0f;
         this.UpdateObject();
     }
 
     // Update is called once per frame
     void Update()
     {
+        if (Input.GetKeyDown(KeyCode.P))
+        {
 
+            //this.UpdateObject();
+            this.UpdateState();
+            this.UpdateObject();
+        }
     }
 }
