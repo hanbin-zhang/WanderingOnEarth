3da6a17bff5c88fd03cf41e07c3e2797f1a6f058
HBZT <78476705+hanbin-zhang@users.noreply.github.com>
2023-03-06 12:19:09 +0000

    add land preparation message

---
+++ Assets/Scripts/Events/Planting/OnLandPrepEvent.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+
+public class OnLandPrepEvent : BaseEvent
+{
+    
+    public interface OnLandPrepListener
+    {
+        public void OnEvent(OnLandPrepMessage msg);
+    }
+
+    public class OnLandPrepMessage : BaseMessage
+    {  
+        public Vector3 pos { get; set; }
+    }
+
+    private List<OnLandPrepListener> listeners = new();
+    private List<Action<OnLandPrepMessage>> actions = new();
+
+    public void AddListener(OnLandPrepListener listener)
+    {
+        listeners.Add(listener);
+    }
+
+    public void AddListener(Action<OnLandPrepMessage> action)
+    {
+        actions.Add(action);
+    }
+
+    public void RemoveListener(OnLandPrepListener listener)
+    {
+        listeners.Remove(listener);
+    }
+
+    public void RemoveListener(Action<OnLandPrepMessage> action)
+    {
+        actions.Remove(action);
+    }
+
+    public void Notify(Vector3 pos)
+    {
+        var msg = new OnLandPrepMessage()
+        {
+            pos = pos
+        };
+
+        listeners.ForEach((x) => x.OnEvent(msg));
+        actions.ForEach((x) => x.Invoke(msg));
+    }
+
+    public void Clear()
+    {
+        listeners.Clear();
+        actions.Clear();
+    }
+
+
+}
+
+
