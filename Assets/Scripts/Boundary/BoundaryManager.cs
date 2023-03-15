0207689d4e9b3b39a39941b1e0b744eca2494270
HBZT <78476705+hanbin-zhang@users.noreply.github.com>
2023-03-15 18:25:53 +0000

    add barriers

---
+++ Assets/Scripts/Boundary/BoundaryManager.cs
@@ -0,0 +1,141 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BoundaryManager : MonoBehaviour
+{
+    public GameObject Boundary;
+
+    private void Start()
+    {
+        GameObjectTracker.boundaryManager = this;
+    }
+
+    public void InstantiateBoundary(StateProperty stateProperty)
+    {
+        int regionSize = Manager.StateController.regionSize;
+        Vector3 originPosition = new Vector3(stateProperty.ColNum * regionSize, 0f, stateProperty.RowNum * regionSize);
+
+        float halfSize = regionSize / 2f;
+        Quaternion rotation = Quaternion.Euler(0f, 90f, 0f);
+
+        Vector3[] positions = new Vector3[] {
+            new Vector3(halfSize, 0f, regionSize) + originPosition,
+            new Vector3(regionSize, 0f, halfSize) + originPosition,
+            new Vector3(halfSize, 0f, 0f) + originPosition,
+            new Vector3(0f, 0f, halfSize) + originPosition
+        };
+
+        Quaternion[] rotations = new Quaternion[] {
+            Quaternion.identity,
+            rotation,
+            Quaternion.identity,
+            rotation
+        };
+
+        for (int i = 0; i < positions.Length; i++)
+        {
+            switch ((BarrierSides)i)
+            {
+                case BarrierSides.FOWARD:
+                    if (originPosition.z + 2 * regionSize > Manager.StateController.mapHeight)
+                    {
+                        stateProperty.boundaries[i] = Instantiate(Boundary, positions[i], rotations[i]);
+                    }
+                    else
+                    {
+                        StateProperty adjacentProperty = Manager.StateController.GetStateProperty(originPosition + new Vector3(0f, 0f, 2 * regionSize));
+                        if (adjacentProperty.boundaries[i + 2] is not null)
+                        {
+                            // Disable the corresponding wall in the adjacent region
+                            adjacentProperty.boundaries[i + 2].SetActive(false);
+                            // Destroy the current wall
+                            Destroy(stateProperty.boundaries[i]);
+                            stateProperty.boundaries[i] = null;
+                            continue; // Skip to the next iteration of the loop
+                        }
+                        else
+                        {
+                            stateProperty.boundaries[i] = Instantiate(Boundary, positions[i], rotations[i]);
+                        }
+                    }
+                    continue;
+
+                case BarrierSides.RIGHT:
+                    if (originPosition.x + 2 * regionSize > Manager.StateController.mapWidth)
+                    {
+                        stateProperty.boundaries[i] = Instantiate(Boundary, positions[i], rotations[i]);
+                    }
+                    else
+                    {
+                        StateProperty adjacentProperty = Manager.StateController.GetStateProperty(originPosition + new Vector3(2 * regionSize, 0f, 0f));
+                        if (adjacentProperty.boundaries[i + 2] is not null)
+                        {
+                            // Disable the corresponding wall in the adjacent region
+                            adjacentProperty.boundaries[i + 2].SetActive(false);
+                            // Destroy the current wall
+                            Destroy(stateProperty.boundaries[i]);
+                            stateProperty.boundaries[i] = null;
+                            continue; // Skip to the next iteration of the loop
+                        }
+                        else
+                        {
+                            stateProperty.boundaries[i] = Instantiate(Boundary, positions[i], rotations[i]);
+                        }
+                    }
+                    continue;
+                case BarrierSides.BACKWARD:
+            
+                    if (originPosition.z - regionSize < 0)
+                    {
+                        stateProperty.boundaries[i] = Instantiate(Boundary, positions[i], rotations[i]);
+                    }
+                    else
+                    {
+                        StateProperty adjacentProperty = Manager.StateController.GetStateProperty(originPosition - new Vector3(0f, 0f, regionSize));
+                        Debug.Log($"new posi{originPosition - new Vector3(regionSize, 0f, 0f)}");
+                        if (adjacentProperty.boundaries[i - 2] is not null)
+                        {
+                            // Disable the corresponding wall in the adjacent region
+                            adjacentProperty.boundaries[i - 2].SetActive(false);
+                            // Destroy the current wall
+                            Destroy(stateProperty.boundaries[i]);
+                            stateProperty.boundaries[i] = null;
+                            continue; // Skip to the next iteration of the loop
+                        }
+                        else
+                        {
+                            stateProperty.boundaries[i] = Instantiate(Boundary, positions[i], rotations[i]);
+                        }
+                    }
+                    continue;
+                case BarrierSides.LEFT:
+                    if (originPosition.x - regionSize < 0)
+                    {
+                        stateProperty.boundaries[i] = Instantiate(Boundary, positions[i], rotations[i]);
+                    }
+                    else
+                    {
+                        StateProperty adjacentProperty = Manager.StateController.GetStateProperty(originPosition - new Vector3(regionSize, 0f, 0f));
+                        if (adjacentProperty.boundaries[i - 2] is not null)
+                        {
+                            // Disable the corresponding wall in the adjacent region
+                            adjacentProperty.boundaries[i - 2].SetActive(false);
+                            // Destroy the current wall
+                            Destroy(stateProperty.boundaries[i]);
+                            stateProperty.boundaries[i] = null;
+                            continue; // Skip to the next iteration of the loop
+                        }
+                        else
+                        {
+                            stateProperty.boundaries[i] = Instantiate(Boundary, positions[i], rotations[i]);
+                        }
+                    }
+                    break;
+                default:
+                    stateProperty.boundaries[i] = Instantiate(Boundary, positions[i], rotations[i]);
+                    continue;
+            }
+        }
+    }
+}
