9ae2842e385c12b8fcc31914262f0a7e0a94fa3f
HBZT <78476705+hanbin-zhang@users.noreply.github.com>
2023-03-23 12:46:52 +0000

    I do not know

--- Assets/Scripts/Events/BaseEvent.cs
+++ Assets/Scripts/Events/BaseEvent.cs
@@ -3,13 +3,15 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Collections;
+using UnityEngine;
 using Photon.Pun;
-
 
 public abstract class BaseEvent { 
     public void NotifyMaster(string className, BaseMessage msg)
     {
         PhotonView view = GameObjectTracker.StateSynchronizer.GetComponent<PhotonView>();
+        Debug.Log(className);
         view.RPC(nameof(sychronizeState.notifyStateMachineRPC), RpcTarget.MasterClient, className, msg);
     }
 }
