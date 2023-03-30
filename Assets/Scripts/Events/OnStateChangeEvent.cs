52522f906043e9cb2a3bdb47cc1d5ad6dd230d49
HBZT <78476705+hanbin-zhang@users.noreply.github.com>
2023-03-30 17:20:20 +0100

    add state change event

---
+++ Assets/Scripts/Events/OnStateChangeEvent.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+
+public class OnStateChangeEvent : BaseEvent
+{
+    public interface OnStateChangeListener
+    {
+        public void OnEvent(OnStateChangeMessage msg);
+    }
+
+    public class OnStateChangeMessage : BaseMessage
+    {
+
+    }
+
+    private List<OnStateChangeListener> listeners = new List<OnStateChangeListener>();
+    private List<Action<OnStateChangeMessage>> actions = new List<Action<OnStateChangeMessage>>();
+
+    public void AddListener(OnStateChangeListener listener)
+    {
+        listeners.Add(listener);
+    }
+
+    public void AddListener(Action<OnStateChangeMessage> action)
+    {
+        actions.Add(action);
+    }
+
+    public void RemoveListener(OnStateChangeListener listener)
+    {
+        listeners.Remove(listener);
+    }
+
+    public void RemoveListener(Action<OnStateChangeMessage> action)
+    {
+        actions.Remove(action);
+    }
+
+    public void Notify()
+    {
+        OnStateChangeMessage msg = new OnStateChangeMessage();
+
+        if (!Photon.Pun.PhotonNetwork.IsMasterClient)
+        {
+        }
+        else
+        {
+            listeners.ForEach((x) => x.OnEvent(msg));
+            actions.ForEach((x) => x.Invoke(msg));
+        }
+    }
+
+    public void Clear()
+    {
+        listeners.Clear();
+        actions.Clear();
+    }
+
+}
