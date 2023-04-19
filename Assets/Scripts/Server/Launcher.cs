f3436a3841ba615283cb7530d5e597cfb2c95153
Nolan <cn752042807@gmail.com>
2023-04-19 21:09:06 +0100

    add snow mountain scene

--- Assets/Scripts/Server/Launcher.cs
+++ Assets/Scripts/Server/Launcher.cs
@@ -87,17 +87,16 @@ public class Launcher : MonoBehaviourPunCallbacks
 
     public override void OnJoinedRoom()
     {
-        int min = 2; // the minimum value of the random integer (inclusive)
-        int max = 4; // the maximum value of the random integer (exclusive)
-
-        int level = Random.Range(min, max);
+        int level = Random.Range(2, 5);
 
         Debug.Log("OnJoinedRoom() called by PUN. Now this client is in a room.");
         if (PhotonNetwork.CurrentRoom.PlayerCount == 1)
         {
-            Debug.LogWarningFormat("We load the World {0}", level);
+            //Debug.LogWarningFormat("We load the World {0}", level);
             
             PhotonNetwork.LoadLevel(level);
+            //test
+            //PhotonNetwork.LoadLevel(4);
         }
     }
 
