d3946bb0f643dfda85b21e665c6b79dd3e4f2853
Nolan <cn752042807@gmail.com>
2023-04-27 18:20:07 +0100

    trigger music by green values

--- Assets/Scripts/Music/MusicController.cs
+++ Assets/Scripts/Music/MusicController.cs
@@ -3,11 +3,35 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class MusicController : MonoBehaviour
-{
-    public AudioSource audio;
-    void Start()
+{ 
+    public AudioClip musicLow;
+    public AudioClip musicMid;
+    public AudioClip musicHigh;
+
+    void Update()
     {
-        audio = GetComponent<AudioSource>();
-        audio.Play();
+        int greenvalue = Manager.GameObjectManager.GetGlobalGreenValue();
+        if (greenvalue <= 100)
+        {
+            GetComponent<AudioSource>().clip = musicLow;
+            Debug.Log("music low");
+        }
+
+        else if(greenvalue >= 500)
+        {
+            GetComponent<AudioSource>().clip = musicHigh;
+            Debug.Log("music high");
+        }
+
+        else
+        {
+            GetComponent<AudioSource>().clip = musicMid;
+            Debug.Log("music mid");
+        }
+
+        if (!GetComponent<AudioSource>().isPlaying)
+        {
+            GetComponent<AudioSource>().Play();
+        }
     }
 }
