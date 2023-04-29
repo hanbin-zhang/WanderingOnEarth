bcf0678943bc0bebb57b662ce0adcbd6659ef82e
HBZT <78476705+hanbin-zhang@users.noreply.github.com>
2023-04-29 13:17:01 +0100

    finish the state tests

--- Assets/Tests/StateMachineTest.cs
+++ Assets/Tests/StateMachineTest.cs
@@ -4,6 +4,7 @@ using NUnit.Framework;
 using UnityEngine;
 using UnityEngine.TestTools;
 
+
 public class StateMachineTest
 {
     // A Test behaves as an ordinary method
@@ -27,15 +28,23 @@ public class StateMachineTest
         Assert.That(Manager.StateController.GetRegionalStateProperty(pos).state, Is.EqualTo(StateLabel.NORMAL));
         Assert.That(stateMsg, Is.Not.Null);
         Assert.That(stateMsg.stateBefore, Is.EqualTo(StateLabel.POLLUTED));
+        Assert.That(stateMsg.stateAfter, Is.EqualTo(StateLabel.NORMAL));
 
         GameObject treePrefab = Resources.Load<GameObject>("Tree");
         for (int i = 0; i < 15; i++)
         {
             GameObject tree = GameObject.Instantiate(treePrefab, pos, Quaternion.identity);
+            tree.name = tree.GetComponent<BaseObject>().GetType().Name;
             Manager.GameObjectManager.Add(tree);
+            Manager.EventController.Get<OnPlantEvent>()?.Notify(
+                tree.transform.position, tree.transform.rotation, tree.name);
         }
-        List<string> keysList = new List<string>(Manager.GameObjectManager.GetAll().Keys);
-        Debug.Log(string.Join(", ", keysList));
+       
+        // test safe change
+        Assert.That(Manager.StateController.GetRegionalStateProperty(pos).state, Is.EqualTo(StateLabel.SAFE));
+        Assert.That(stateMsg.stateBefore, Is.EqualTo(StateLabel.NORMAL));
+        Assert.That(stateMsg.stateAfter, Is.EqualTo(StateLabel.SAFE));
+
     }
     // A UnityTest behaves like a coroutine in Play Mode. In Edit Mode you can use
     // `yield return null;` to skip a frame.
