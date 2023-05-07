d70f373e4c09f41e24613ae9773d16e471757b60
Nolan-Nan <cn752042807@gmail.com>
2023-05-07 17:24:26 +0100

    skybox

--- Assets/Scripts/Music&Sky/MusicController.cs
+++ Assets/Scripts/Music&Sky/MusicController.cs
@@ -7,6 +7,9 @@ public class MusicController : MonoBehaviour
     public AudioClip musicLow;
     public AudioClip musicMid;
     public AudioClip musicHigh;
+    public Material skybox1;
+    public Material skybox2;
+    public Material skybox3;
 
     void Update()
     {
@@ -14,18 +17,21 @@ public class MusicController : MonoBehaviour
         if (greenvalue <= 100)
         {
             GetComponent<AudioSource>().clip = musicLow;
+            RenderSettings.skybox = skybox1;
             Debug.Log("music low");
         }
 
         else if(greenvalue >= 500)
         {
             GetComponent<AudioSource>().clip = musicHigh;
+            RenderSettings.skybox = skybox3;
             Debug.Log("music high");
         }
 
         else
         {
             GetComponent<AudioSource>().clip = musicMid;
+            RenderSettings.skybox = skybox2;
             Debug.Log("music mid");
         }
 
